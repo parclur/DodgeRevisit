5b4f7ce275151deee008ebfb3b0ff3a251b41793
claire.yeash <claire.yeash@02d9ae83-e185-4882-b862-177a0222efca>
2018-05-17 18:58:20 +0000

    Getting the menus to work

--- Unity Files/Dodge Game/Assets/Scripts/RewiredPlayerLoginManager.cs
+++ Unity Files/Dodge Game/Assets/Scripts/RewiredPlayerLoginManager.cs
@@ -8,12 +8,12 @@ public class RewiredPlayerLoginManager : MonoBehaviour {
     public int playerId;
     private Rewired.Player player;
 
-    public bool p1WaitingToJoin, p1CharacterSelect, p1ReadyToPlay;
-    public bool p2WaitingToJoin, p2CharacterSelect, p2ReadyToPlay;
-    public bool p3WaitingToJoin, p3CharacterSelect, p3ReadyToPlay;
-    public bool p4WaitingToJoin, p4CharacterSelect, p4ReadyToPlay;
+    public int p1State, p2State, p3State, p4State;
 
-    public GameObject player1Panel;
+    public GameObject p1JoinText;
+    public GameObject p1Panel;
+    public GameObject p1ReadyText;
+
     public bool p1IsStriker;
     public GameObject p1StrikerCharacter;
     public GameObject p1BlockerCharacter;
@@ -23,14 +23,97 @@ public class RewiredPlayerLoginManager : MonoBehaviour {
     void Start()
     {
         player = ReInput.players.GetPlayer(playerId);
+
+        p1State = 0;
+        p2State = 0;
+        p3State = 0;
+        p4State = 0;
     }
 
     void Update()
     {
-        if (player.GetButtonDown("Submit"))
+        if (player.GetButtonDown("AButton"))
         {
-            Debug.Log(playerId + " Fire!");
-            //switch
+            Debug.Log(playerId + " Advance");
+            switch (playerId)
+            {
+                //Player 1
+                case 0:
+                    p1State++;
+                    break;
+                //Player 2
+                case 1:
+                    p2State++;
+                    break;
+                //Player 3
+                case 2:
+                    p3State++;
+                    break;
+                //Player 4
+                case 3:
+                    p4State++;
+                    break;
+            }
+            PlayerState();
+        }
+
+        if (player.GetButtonDown("BButton"))
+        {
+            Debug.Log(playerId + " Back");
+            switch (playerId)
+            {
+                //Player 1
+                case 0:
+                    p1State--;
+                    break;
+                //Player 2
+                case 1:
+                    p2State--;
+                    break;
+                //Player 3
+                case 2:
+                    p3State--;
+                    break;
+                //Player 4
+                case 3:
+                    p4State--;
+                    break;
+            }
+            PlayerState();
+        }
+    }
+
+    void PlayerState()
+    {
+        switch (p1State)
+        {
+            case -1:
+                p1State = 0;
+                break;
+            //waiting to join
+            case 0:
+                Debug.Log("State: " + p1State);
+                //show "Press A to Join"
+                p1JoinText.SetActive(true);
+                p1Panel.SetActiveRecursively(false);
+                break;
+            //character select
+            case 1:
+                Debug.Log("State: " + p1State);
+                //show character panel
+                p1JoinText.SetActive(false);
+                p1Panel.SetActiveRecursively(true);
+                p1ReadyText.SetActive(false);
+                break;
+            //ready
+            case 2:
+                Debug.Log("State: " + p1State);
+                //show ready text and advance if all joined players are
+                p1ReadyText.SetActive(true);
+                break;
+            case 3:
+                p1State = 2;
+                break;
         }
     }
 }
